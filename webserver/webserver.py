c7c5a0c1b6d1595a1c87db4a9316d8f94dad21f9
Iulian Stana <julian.stana@gmail.com>
2019-03-24 21:57:22 +0200

    Create a light flask server to call weather service using python client

---
+++ webserver/webserver.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+tmp_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+if sys.path[0] != tmp_path:
+    while tmp_path in sys.path:
+        sys.path.remove(tmp_path)
+    sys.path.insert(0, tmp_path)
+
+from flask import Flask, jsonify
+from flask import request
+from flask import abort
+from flask import make_response
+
+from clients.weather_service.python.weather_client import WeatherClient
+
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Handler 404 error in a better way """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def not_found(error):
+    """ Handler 400 error in a better way """
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.route('/weather/', methods=['GET'])
+def get_weather():
+    """
+    Use GET method to get today weather value for a given city
+    """
+    city = request.args.get('city')
+
+    if city is None:
+        abort(400, "Need to provide a city.")
+
+    # get weather
+    client = WeatherClient()
+
+    return jsonify(client.get({'city': city}))
+
+
+if __name__ == '__main__':
+    print "Start flask server"
+    app.run(host="0.0.0.0", port=5000, debug=True)
