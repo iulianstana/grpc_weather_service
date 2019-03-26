c63963783d64e4c326252fec72f141d670385d9c
Iulian Stana <julian.stana@gmail.com>
2019-03-26 23:38:00 +0200

    Use accuweather API to get current conditions for a given query

--- weather_service_server/weather_service_service.go
+++ weather_service_server/weather_service_service.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 
 	pb "weather_service/protos/go/weather"
+	weatherAPI "weather_service/weather_service_server/driver"
 )
 
 const (
@@ -14,11 +16,33 @@ const (
 )
 
 // server is used to implement weather.WeatherServiceServer.
-type server struct{}
+type server struct {
+	weatherDriver *weatherAPI.AccuweatherAPI
+}
+
+func NewServer() *server {
+	weatherDriver := weatherAPI.NewAccuweatherAPIDriver()
+	return &server{weatherDriver: weatherDriver}
+}
 
 func (s *server) Get(ctx context.Context, in *pb.WeatherRequest) (*pb.WeatherResponse, error) {
 	log.Printf("Received: %v", in.City)
-	return &pb.WeatherResponse{Temperature: 30.5}, nil
+
+	locations, err := s.weatherDriver.GetLocations(in.City)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(locations)
+	firstLocation := locations[0]
+
+	currentConditions, err := s.weatherDriver.GetCurrentConditions(firstLocation.Key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(currentConditions)
+	firstCurrentCondition := currentConditions[0]
+
+	return &pb.WeatherResponse{Temperature: firstCurrentCondition.Temperature.Metric.Value}, nil
 }
 
 func main() {
@@ -26,8 +50,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	serverObj := NewServer()
+
 	s := grpc.NewServer()
-	pb.RegisterWeatherServiceServer(s, &server{})
+
+	pb.RegisterWeatherServiceServer(s, serverObj)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
